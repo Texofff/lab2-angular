fdcaf2ff0f827a7954ff3507aea62f6e95851750
Texoff <149324458+Texofff@users.noreply.github.com>
2024-09-27 10:55:21 +0300

    Add files via upload

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Phone pro XL',
+    price: 799,
+    description: 'Покращений телефон про версії'
+  },
+  {
+    name: 'Phone pro Mini',
+    price: 599,
+    description: 'Покращений телефон про версії зменшеної діагоналі'
+  },
+  {
+    name: 'Phone Standard',
+    price: 399,
+    description: 'Телефон стандартної версії'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
